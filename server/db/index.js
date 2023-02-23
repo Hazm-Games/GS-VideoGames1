c1fcb716b882f38f108c06d33911895bdf8702b6
Angelica1970 <angelbaldwin013@gmail.com>
2023-02-23 09:42:48 -0600

    seedData update

--- server/db/index.js
+++ server/db/index.js
@@ -6,12 +6,40 @@ const {
 } = require('./User');
 
 const syncTables = async()=> {
+  console.log("syncing tables");
   const SQL = `
+  DROP TABLE IF EXISTS Cart_Products;
+  DROP TABLE IF EXISTS Cart;
+  DROP TABLE IF EXISTS Products;
+  DROP TABLE IF EXISTS Platform;
   DROP TABLE IF EXISTS users;
   CREATE TABLE users(
     id SERIAL PRIMARY KEY,
     username VARCHAR(100) UNIQUE NOT NULL,
     password VARCHAR(100) NOT NULL
+  );
+  CREATE TABLE products(
+    id  SERIAL  PRIMARY KEY,
+    name  VARCHAR(255)  UNIQUE NOT NULL,
+    description TEXT  NOT NULL,
+    price DECIMAL(10, 2) NOT NULL,
+    image_url VARCHAR(255) NOT NULL,
+    condition VARCHAR(10) NOT NULL
+   );
+   CREATE TABLE Cart(
+    id  SERIAL  PRIMARY KEY,
+    "user_id" INTEGER NOT NULL REFERENCES Users(id),
+    created_at TIMESTAMP NOT NULL DEFAULT NOW()
+   );
+   CREATE TABLE Cart_Products(
+    id  SERIAL  PRIMARY KEY,
+    "cart_id" INTEGER NOT NULL REFERENCES Cart(id),
+    "product_id" INTEGER NOT NULL REFERENCES Products(id),
+    quantity INTEGER NOT NULL
+  );
+  CREATE TABLE Platform(
+    id  SERIAL  PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
   );
   `;
   await client.query(SQL);
@@ -35,10 +63,10 @@ const syncAndSeed = async()=> {
 };
 
 
-module.exports = {
+ module.exports = {
   syncAndSeed,
   createUser,
   authenticate,
   getUserByToken,
   client
-};
+ };
