5f4dabee2663215e87e163b2c0f41b1176e1978d
Matthewdev-prog <114083484+Matthewdev-prog@users.noreply.github.com>
2023-03-08 11:47:04 -0500

    Update cart.js
    hunters

--- server/db/cart.js
+++ server/db/cart.js
@@ -12,6 +12,26 @@ const createCart = async (userId) => {
   }
 };
 
+// get cart by user id
+const getCartByUserId = async ({ userId }) => {
+  const SQL = `
+    SELECT * FROM carts
+    WHERE user_id = $1 AND is_active = true;
+  `;
+  const response = await client.query(SQL, [userId]);
+  const cart = response.rows[0];
+  // get products, and attach to cart
+  const productsSQL = `
+  SELECT * FROM cart_products
+  LEFT JOIN products ON cart_products.product_id = products.id
+  WHERE cart_products.cart_id = $1
+  `;
+  const productsResponse = await client.query(productsSQL, [cart.id]);
+  cart.products = productsResponse.rows;
+  return cart;
+};
+
+
 // function to add a product to a cart
 const addProductToCart = async (cartId, productId, quantity) => {
   try {
@@ -22,23 +42,6 @@ const addProductToCart = async (cartId, productId, quantity) => {
     `;
     const response = await client.query(SQL, [cartId, productId, quantity]);
     return response.rows[0];
-  } catch (error) {
-    console.error(error);
-    throw error;
-  }
-};
-
-// function to get all products in a cart
-const getCartProducts = async (cartId) => {
-  try {
-    const SQL = `
-      SELECT p.*, cp.quantity
-      FROM products p
-      LEFT JOIN cart_Products cp ON p.id = cp.product_id
-      WHERE cp.cart_id = $1;
-    `;
-    const response = await client.query(SQL, [cartId]);
-    return response.rows;
   } catch (error) {
     console.error(error);
     throw error;
@@ -76,12 +79,25 @@ const deleteCartProduct = async (cartProductId) => {
   }
 };
 
-//Create Purchase Cart function TODO 
+ 
+// purchase cart
+const purchaseCart = async ({ cartId, userId }) => {
+  const SQL = `
+  UPDATE carts
+  SET is_active = false
+  WHERE id = $1
+  `;
+  await client.query(SQL, [cartId]);
+  const newCart = await createCart({ userId });
+  return newCart;
+};
+
 
 module.exports = {
   createCart,
   addProductToCart,
-  getCartProducts,
+  getCartByUserId,
   updateCartProductQuantity,
   deleteCartProduct,
+  purchaseCart,
 };
