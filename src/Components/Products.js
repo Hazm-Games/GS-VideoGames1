c28c169a1f308abf0b000f310f7c9365f5e0c248
zuly11 <zuly.rod.11@gmail.com>
2023-03-15 11:23:23 -0500

    added cards to the products page

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -5,7 +5,7 @@ const Products = ({ products }) => {
   const navigate = useNavigate();
   const { term } = useParams();
   return (
-  
+    <div className="games-shelf">
     <ul>
      <h2 className="search-title">Search for a game</h2>
       <input
@@ -15,20 +15,30 @@ const Products = ({ products }) => {
           console.log(ev.target.value);
         }}
       />
+      <ul className="game-grid">
       {products
         .filter((product) => {
           return !term || product.name.includes(term);
         })
+        
         .map((product) => {
           return (
-            <li key={product.id}>
+            <li className="nostyle" key={product.id}>
               <h3>
-                <Link to={`/products/${product.id}`}>{product.name}</Link>
+                <Link to={`/products/${product.id}`}>
+                <div className="game-card">
+                <img src={product.image_url} />
+                {product.name}
+
+                </div>
+                </Link>
               </h3>
             </li>
           );
         })}
+        </ul>
     </ul>
+    </div>
   );
 };
 
